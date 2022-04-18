2ef417eaff3eab78369a3426c69ed43782c23812
Cedcn <cedcn@qq.com>
2022-04-19 00:45:41 +0800

    Refactor dynamic menus

--- chrome/extension/background/contextMenus.js
+++ chrome/extension/background/contextMenus.js
@@ -1,8 +1,10 @@
-import { reduce, forEach, get, map } from 'lodash'
+import { reduce, forEach, get, map, find } from 'lodash'
 import parse from 'url-parse'
 import getCurrentMileStoneStr from '../../../app/utils/getCurrentMileStoneStr'
 import generateId from '../../../app/utils/generateId'
 import { initFiller, initActions, initToolbar } from '../utils'
+
+let dynamicMenus = []
 
 const updateDynamicMenus = (actions) => {
   return map(actions, (action) => {
@@ -16,73 +18,86 @@ const updateDynamicMenus = (actions) => {
       documentUrlPatterns: ['http://*/*', 'https://*/*'],
     })
 
-    const listener = (params) => {
-      if (params.menuItemId === menuItemId) {
-        const replaceMatchers = [
-          {
-            regex: /\$\{m\}/,
-            value: getCurrentMileStoneStr(),
-          },
-          {
-            regex: /\$\{m-1\}/,
-            value: getCurrentMileStoneStr(-1),
-          },
-          {
-            regex: /\$\{m\+1\}/,
-            value: getCurrentMileStoneStr(1),
-          },
-        ]
-
-        const targetUrl = reduce(replaceMatchers, (acc, cur) => acc.replace(cur.regex, cur.value), action.action.value)
-        const targetParsed = parse(targetUrl)
-        chrome.tabs.query({ active: true }, (tabs) => {
-          const activeUrl = tabs[0].url
-
-          if (activeUrl && activeUrl.match(targetParsed.hostname)) {
-            chrome.tabs.update({ url: targetUrl })
-          } else {
-            chrome.tabs.create({ url: targetUrl })
-          }
-        })
-      }
-    }
-
-    chrome.contextMenus.onClicked.addListener(listener)
-    return { menuItemId, listener }
+    return { menuItemId, ...action }
   })
 }
-
-let actionsMenu = []
-chrome.storage.onChanged.addListener((changes) => {
+//
+const storageChangedListener = (changes) => {
   if ('actions' in changes) {
     const storageChange = changes.actions
     const newActions = storageChange.newValue
 
     new Promise((reslove) => {
       let i = 0
-      forEach(actionsMenu, (menu) => {
+      forEach(dynamicMenus, (menu) => {
         chrome.contextMenus.remove(menu.menuItemId, () => {
-          chrome.contextMenus.onClicked.removeListener(menu.listener)
-
-          if (++i >= actionsMenu.length - 1) {
+          if (++i >= dynamicMenus.length - 1) {
             reslove()
           }
         })
       })
     }).then(() => {
-      actionsMenu = updateDynamicMenus(newActions)
+      dynamicMenus = updateDynamicMenus(newActions)
     })
   }
-})
+}
 
+chrome.storage.onChanged.removeListener(storageChangedListener)
+chrome.storage.onChanged.addListener(storageChangedListener)
+
+//
 const FORM_FILL_MENU_ID = 'form_fill_menu'
-
-chrome.contextMenus.onClicked.addListener((params, tab) => {
+const contextMenusListener = (params, tab) => {
   if (params.menuItemId === FORM_FILL_MENU_ID) {
     chrome.tabs.sendMessage(tab.id, 'fillMenuClicked')
   }
-})
+}
 
+chrome.contextMenus.onClicked.removeListener(contextMenusListener)
+chrome.contextMenus.onClicked.addListener(contextMenusListener)
+
+//
+const dynamicMenusListener = (params) => {
+  const selectedMenu = find(dynamicMenus, (menu) => menu.menuItemId === params.menuItemId)
+
+  if (selectedMenu) {
+    const replaceMatchers = [
+      {
+        regex: /\$\{m\}/,
+        value: getCurrentMileStoneStr(),
+      },
+      {
+        regex: /\$\{m-1\}/,
+        value: getCurrentMileStoneStr(-1),
+      },
+      {
+        regex: /\$\{m\+1\}/,
+        value: getCurrentMileStoneStr(1),
+      },
+    ]
+
+    const targetUrl = reduce(
+      replaceMatchers,
+      (acc, cur) => acc.replace(cur.regex, cur.value),
+      selectedMenu.action.value
+    )
+    const targetParsed = parse(targetUrl)
+    chrome.tabs.query({ active: true }, (tabs) => {
+      const activeUrl = tabs[0].url
+
+      if (activeUrl && activeUrl.match(targetParsed.hostname)) {
+        chrome.tabs.update({ url: targetUrl })
+      } else {
+        chrome.tabs.create({ url: targetUrl })
+      }
+    })
+  }
+}
+
+chrome.contextMenus.onClicked.removeListener(dynamicMenusListener)
+chrome.contextMenus.onClicked.addListener(dynamicMenusListener)
+
+//
 const installedHandler = async ({ reason }) => {
   chrome.contextMenus.create({
     id: FORM_FILL_MENU_ID,
@@ -98,14 +113,14 @@ const installedHandler = async ({ reason }) => {
 
   chrome.storage.local.get('actions', (value) => {
     const actions = get(value, 'actions', [])
-    actionsMenu = updateDynamicMenus(actions)
+    dynamicMenus = updateDynamicMenus(actions)
   })
 
   if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
     await initFiller()
     await initToolbar()
     initActions().then((actions) => {
-      actionsMenu = updateDynamicMenus(actions)
+      dynamicMenus = updateDynamicMenus(actions)
     })
   }
 }
